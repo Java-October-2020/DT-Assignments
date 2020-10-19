ec0246b73172a5950a7fffedacfc1604c6b122f3
DT <deep7dt@gmail.com>
2020-10-19 19:15:15 -0400

    update of Spring Boot Assignments

--- 04-java-spring/01-spring-fundamentals/03-hello-human/HelloHuman/src/main/Webapp/WEB-INF/index.jsp
+++ 04-java-spring/01-spring-fundamentals/03-hello-human/HelloHuman/src/main/Webapp/WEB-INF/index.jsp
@@ -1,15 +1,14 @@
- <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+
     <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>DT's Hello Human</title>
-<link rel="stylesheet" type = "text/css" href = "css/style.css">
+
 </head>
 <body>
-<h1>Hello ${name}</h1>
+<h1>Hello Human!</h1>
 <p>Welcome to SpringBoot!</p>
 
 </body>
--- 04-java-spring/01-spring-fundamentals/03-hello-human/HelloHuman/src/main/java/com/cd/dtstanley/hellohuman/HelloHumanApplication.java
+++ 04-java-spring/01-spring-fundamentals/03-hello-human/HelloHuman/src/main/java/com/cd/dtstanley/hellohuman/HelloHumanApplication.java
@@ -2,10 +2,14 @@ package com.cd.dtstanley.hellohuman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@SuppressWarnings("unused")
 @SpringBootApplication
 public class HelloHumanApplication {
 
+	//Entry Point Method - it allows this program to run
 	public static void main(String[] args) {
 		SpringApplication.run(HelloHumanApplication.class, args);
 	}
